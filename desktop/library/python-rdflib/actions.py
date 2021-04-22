0d923bbd0b5192db06c793dcb1df33bda0e20f79
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-04-23 01:26:05 +0300

    Added various packages

---
+++ desktop/library/python-rdflib/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def setup():
+    pythonmodules.compile(pyVer = "3")
+    pythonmodules.compile()
+
+
+def install():
+    pythonmodules.install()
+    pythonmodules.install(pyVer = "3")
+
