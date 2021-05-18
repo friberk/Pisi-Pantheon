75e790168293b4220cc92db9935d6161a4ac70e7
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-05-18 22:54:26 +0300

    Various updates

--- desktop/dm/lightdm/actions.py
+++ desktop/dm/lightdm/actions.py
@@ -1,28 +1,29 @@
-
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
-    #shelltools.system("sed -i -e 's:getgroups:lightdm_&:' tests/src/libsystem.c")
-    #shelltools.system("sed -i s/systemd/elogind/ data/pam/*")
-    
-    autotools.configure("--prefix='/usr' --sbindir='/usr/bin' --sysconfdir='/etc' \
-                         --localstatedir='/var' --libexecdir='/usr/lib/lightdm' \
-                         --with-greeter-user='root' \
-                         --with-greeter-session='lightdm-gtk-greeter' \
-                         --disable-static \
-                         --enable-vala")
+    shelltools.system("NOCONFIGURE=1 ./autogen.sh")
+    autotools.configure(" --prefix=/usr \
+                          --libexecdir=/usr/lib/lightdm \
+                          --localstatedir=/var \
+                          --sbindir=/usr/bin \
+                          --sysconfdir=/etc \
+                          --disable-static \
+                          --disable-tests \
+                          --enable-gtk-doc \
+                          --with-greeter-user=lightdm")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
